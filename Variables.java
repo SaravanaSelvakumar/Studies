b0277de612508cb1dfde528ee73d5b074040ac6f
Saravanan S <130735484+SaravanaSelvakumar@users.noreply.github.com>
2024-03-06 11:24:24 +0530

    Update Variables.java
    swap to values

--- Variables.java
+++ Variables.java
@@ -1,13 +1,24 @@
 public class Variables {
     public static void main(String[] args) {
+        int sum1 = 10;
+        int sum2 = 5;
+        int sum3;
+        sum3 = sum1;
+        sum1 = sum2;
+        sum2 = sum3;
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+
         String a = "water";
         String b = "juice";
         String temp;
-
         temp = a;
         a = b;
         b = temp;
+
         System.out.println("x : " + a);
         System.out.println("y : " + b);
+
     }
 }
